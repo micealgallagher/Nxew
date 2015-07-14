4e422ec243b9c4823433d4a1055f69d99f5e30e0
Miceal Gallagher <me@mehaul.me>
2015-07-14 01:17:41 -0400

    Modify general appearance

--- views/site/login.php
+++ views/site/login.php
@@ -7,28 +7,24 @@ use yii\bootstrap\ActiveForm;
 /* @var $model \common\models\LoginForm */
 
 $this->title = 'Login';
-?>     
+?>
 
-
-<div class="row">
-    <div class="col-xs-12 col-md-12 text-center title-bottom-padding">
-        <h1><?= Html::encode($this->title) ?></h1>
-    </div>
-</div>
-
-<div class="row">
-  <div class="col-xs-6 col-md-4"></div>
-  <div class="col-xs-6 col-md-4 text-center">
-    <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+<div class="row center-block" >
+    <div class="col-xs-4 col-xs-offset-4 col-lg-4 col-lg-offset-4">
+        <div class="panel panel-default">
+            <div class="panel-heading"><h1><?= Html::encode($this->title) ?></h1></div>
+            <div class="panel-body">
+                <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
                 <?= $form->field($model, 'username') ?>
                 <?= $form->field($model, 'password')->passwordInput() ?>
                 <div class="form-group">
                     <?= Html::submitButton('Login', ['class' => 'btn btn-success', 'name' => 'login-button', 'style' => 'width: 100%']) ?>
                 </div>
-                                <div style="color:white;margin:1em 0">
+                <div style="margin:1em 0">
                     If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset'], ['style' => 'color: orange']) ?>.
                 </div>
-            <?php ActiveForm::end(); ?>
-  </div>
-  <div class="col-xs-6 col-md-4"></div>
+                <?php ActiveForm::end(); ?>
+            </div>
+        </div>
+    </div>
 </div>
